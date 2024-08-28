7934340a5ee08a1b432e2daf2da06f780c181513
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-08-28 14:10:16 -0300

    update interface

--- preload.js
+++ preload.js
@@ -4,5 +4,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
   downloadVideo: (data) => ipcRenderer.invoke('download-video', data),
   onDownloadProgress: (callback) => ipcRenderer.on('download-progress', callback),
   onDownloadComplete: (callback) => ipcRenderer.on('download-complete', callback),
-  onDownloadError: (callback) => ipcRenderer.on('download-error', callback)
+  onDownloadError: (callback) => ipcRenderer.on('download-error', callback),
+  openFolderDialog: () => ipcRenderer.invoke('open-folder-dialog'),
+  minimizeWindow: () => ipcRenderer.send('minimize-window'),
+  maximizeWindow: () => ipcRenderer.send('maximize-window'),
+  closeWindow: () => ipcRenderer.send('close-window')
 });
